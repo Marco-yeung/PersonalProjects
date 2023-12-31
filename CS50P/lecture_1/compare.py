15d3b2e8acfeba344d23c5e09b2fac57f2b6f32c
Marco-yeung <shuto.yeung933@gmail.com>
2023-12-31 22:30:08 +0800

    create a compare script

---
+++ CS50P/lecture_1/compare.py
@@ -0,0 +1,21 @@
+x = input("What is x? ")
+y = input("What is y? ")
+
+# if x > y:
+#     print(f"{x} is greater than {y}")
+    
+# elif x < y:
+#     print(f"{x} is less than {y}")
+    
+# else:
+#     print(f"{x} is equal to {y}")
+
+# if x < y or x > y:
+#     print("x is not equal to y")
+# else:
+#     print("x is equal to y")
+
+if x == y:
+    print("x is equal to y")
+else:
+    print("x is not equal to y")
