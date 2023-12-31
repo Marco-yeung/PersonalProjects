23860b399b23f7d1ed0bd961bd5277a64a211a73
Marco-yeung <shuto.yeung933@gmail.com>
2023-12-31 21:57:10 +0800

    create a calculator

---
+++ CS50P/lecture_0/calculator.py
@@ -0,0 +1,12 @@
+x = float(input("What is x? "))
+y = float(input("What is y? "))
+
+# rounding to 2 dp
+# z = round(x + y, 2)
+# print(z)
+# OR
+z = x / y
+print(f"{z:.2f}")
+
+# a more simplified version to do it would be
+# print(int(input("What is x? ")) + int(input("What is y? ")))
