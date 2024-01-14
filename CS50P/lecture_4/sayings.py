7ca612408654d390a0e63a63c32e6f00bd0e83a9
Marco-yeung <shuto.yeung933@gmail.com>
2024-01-14 21:21:58 +0800

    create a sayings script

---
+++ CS50P/lecture_4/sayings.py
@@ -0,0 +1,15 @@
+# Here we are creating our own module, with a hello and goodbye function
+# this would be associated with the say script
+def main():
+    hello("world")
+    goodbye("world")
+
+
+def hello(name):
+    print(f"Hello, {name}")
+    
+def goodbye(name):
+    print(f"Hello, {name}")
+    
+main()
+
