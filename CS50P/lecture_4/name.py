c7d8ca2a0bb6c91a0282e50d1a61c8e2c09cb478
Marco-yeung <shuto.yeung933@gmail.com>
2024-01-14 19:34:57 +0800

    add more info to name file

--- CS50P/lecture_4/name.py
+++ CS50P/lecture_4/name.py
@@ -8,11 +8,23 @@ import sys
 # except IndexError:
 #     print("You have to few arguments")
 
+
+# sys.exit() here could exit the program without printing anything below
 if len(sys.argv) < 2:
-    print("You have to few arguments")
-elif len(sys.argv) > 2:
-    print("You have to many arguments")
-else:
-    print("Hello, my name is", sys.argv[1])
+    sys.exit("You have to few arguments")
+# elif len(sys.argv) > 2:
+#     sys.exit("You have to many arguments")
+
+# print("Hello, my name is", sys.argv[1])
+
+for arg in sys.argv[1:]:
+    print("Hello, my name is", arg)
+    
+    
+# A third party library in python would be called packages
+# packages would be a module implemented in a folder, not just a file
+# A package would be a library that we can install in our own PC
+
+# pip would be a common command to install package online to our own PC
     
 
