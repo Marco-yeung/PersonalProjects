d88b6b33afb4a5a2a2caf044c123e6e1c1330e4e
Marco-yeung <shuto.yeung933@gmail.com>
2024-01-14 21:36:51 +0800

    adding more comment on name = main

--- CS50P/lecture_4/say.py
+++ CS50P/lecture_4/say.py
@@ -1,4 +1,6 @@
 import sys
+# it will cause python to find sayings.py, read it from top to bottom, left to right. it is going to ignore the call to main this time. It is because it is wrap inside the conditional
+# In this case, when we are importing a file, and not directly running it in the command line, main will not get call
 from sayings import hello
 
 if len(sys.argv) == 2:
