f27dce1547ba55fb5a4bfaf2869b20d692d1de78
Marco-yeung <shuto.yeung933@gmail.com>
2024-03-03 17:56:11 +0800

    upload all changes

---
+++ CS50P/assignments/problem_set_6/lines.py
@@ -0,0 +1,16 @@
+import sys
+def main():
+    lines(input(""))
+
+def lines():
+    try:
+        if len(sys.argv) <= 1:
+            sys.exit("Too few command-line arguments")
+        elif len(sys.argv) > 1:
+            sys.exit("Too many command-line arguments")
+        elif sys.argv[1].endswith
+    except:
+
+
+if __name__ == "__main__":
+    main()
