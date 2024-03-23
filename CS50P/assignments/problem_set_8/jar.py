0e9dbf3df2a582e6cd299d17c33ab504fde0117e
Marco-yeung <shuto.yeung933@gmail.com>
2024-03-23 19:13:23 +0800

    problem set 8 script upload

--- CS50P/assignments/problem_set_8/jar.py
+++ CS50P/assignments/problem_set_8/jar.py
@@ -10,24 +10,128 @@
 - size: should return the number of cookie actually in the cookie jar, initially 0
 
 """
-
+# my own code
+"""
 class Jar:
-    def __init__(self, capacity=12):
+    def __init__(self, capacity=12, actual = 0):
         self.capacity = capacity
+        self.actual = actual
 
     def __str__(self):
-        ...
+        return f"Jar: ", {self.actual} * "🍪"
 
     def deposit(self, n):
-        self.capacity += n
-
+        if self.actual + n < self.capacity:
+            self.actual += n
+        else:
+            print('The jar is full that it cant contain that much cookie')
     def withdraw(self, n):
-        self.capacity -= n
-
+        if n < self.actual:
+            self.actual -= n
+        else:
+            print("The jar doesn't have that much cookies")
+    # getter for capacity
     @property
     def capacity(self):
-        return self.capacity
+        return self._capacity
+    # setter for capacity
+    @capacity.setter
+    def capacity(self, capacity):
+        if capacity > 0:
+            self._capacity = capacity
+        else:
+            raise ValueError("Capacity must be a positive integer")
 
     @property
     def size(self):
-        return 
+        return self._actual
+    
+    @size.setter
+    def size(self, size):
+        if size >= 0:
+            self._actual = size
+        else:
+            raise ValueError("Size must be a non-negative integer")
+        
+    """
+
+# tutor code
+"""
+
+class Jar:
+    def __init__(self, capacity=12):
+        if capacity < 0:
+            raise ValueError("Wrong capacity")
+        self._capacity = capacity
+        self._size = 0
+
+    def __str__(self):
+        return self.size * "🍪"
+
+    def deposit(self, n):
+        if n > self.capacity:
+            raise ValueError("Invalid capacity")
+        if n + self.size > self.capacity:
+            raise ValueError("Not enough capacity")
+        self.size += n
+        
+
+    def withdraw(self, n):
+        if self.size - n < 0:
+            raise ValueError("Not enough cookies to remove")
+        self._size -= n
+
+    @property
+    def capacity(self):
+        return self._capacity
+
+    @property
+    def size(self):
+        return self._size
+        
+jar = Jar()
+print(jar)
+
+"""
+
+class Jar:
+    def __init__(self, capacity=12)-> None:
+        self.capacity = capacity
+        self.size = 0
+
+    def __str__(self) -> str:
+        return self.size * "🍪"
+    
+    def deposit(self, n:int)-> None:
+        self.size += n
+    
+    def withdraw(self, n:int)-> None:
+        self.size -= n
+
+    # getter of capacity
+    @property
+    def capacity(self) -> int:
+        return self._capacity
+    
+    @capacity.setter
+    def capacity(self, capacity:int) -> None:
+        if capacity < 0:
+            raise ValueError("Invalid capacity of the jar")
+        self._capacity = capacity
+
+    # getter of size
+    @property
+    def size(self) -> int:
+        return self._size
+    
+    @size.setter
+    def size(self, size:int)-> None:
+        if size < 0:
+            raise ValueError("Too few cookies")
+        if size > self.capacity:
+            raise ValueError("Too many cookies")
+        self._size = size
+        
+        
+
+    
--- CS50P/assignments/problem_set_8/shirtificate.py
+++ CS50P/assignments/problem_set_8/shirtificate.py
@@ -1,7 +1,34 @@
+"""
 from fpdf import FPDF
 
-pdf = FPDF()
+pdf = FPDF(orientation="P", unit="mm", format="A4")
 pdf.add_page()
-pdf.set_font("helvetica", "B", 16)
-pdf.cell(40, 10, "Hello World!")
+pdf.set_font("helvetica",'b',  30)
+pdf.cell(210, 50, "CS50 Shirtificate", align = 'c')
+image = "./shirtificate.png"
+pdf.image(image, x = 60, y = 60, w = 100)
+pdf.set_text_color(255,255,255)
+pdf.cell(210, 150, "John Harvard took CS50", align = 'c')
 pdf.output("shirtificate.pdf")
+
+"""
+
+from fpdf import FPDF
+
+class PDF():
+    def __init__(self, name):
+        self._pdf = FPDF()
+        self._pdf.add_page()
+        self._pdf.set_font("helvetica",'b',  30)
+        self._pdf.cell(0, 60, 'CS50 Shirtificate', new_x="LMARGIN", new_y="NEXT", align='C')
+        self._pdf.image("./shirtificate.png", w = self._pdf.epw)
+        self._pdf.set_font_size(30)
+        self._pdf.set_text_color(255,255,255)
+        self._pdf.text(x=47.5, y = 140, txt = f"{name} took CS50")
+
+    def save(self, name):
+        self._pdf.output(name)
+
+name = input("Name: ")
+pdf = PDF(name)
+pdf.save("shirtificate.pdf")
