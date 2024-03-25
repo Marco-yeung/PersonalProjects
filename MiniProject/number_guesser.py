1c8bf728d315a780da2ebfade0f41a9a4e16eadb
Marco-yeung <shuto.yeung933@gmail.com>
2024-03-25 23:13:30 +0800

    create number_guesser and rock_paper_scissors script

---
+++ MiniProject/number_guesser.py
@@ -0,0 +1,7 @@
+"""
+1. create a number guesser function that let user input a number, count their guesses and return how many times for them to get the correct answer. we could generate our correct answer randomly
+2. could set some condition to let user know is higher or lower than the correct answer
+3
+
+"""
+
---
+++ MiniProject/rock_paper_scissors.py
@@ -0,0 +1,6 @@
+"""
+1. create a function that store rock, paper, scissors into a list, then can access the answer with indexing the list
+2. set a random number for the computer, let user input then play with the computer, then count the wins and losses 
+3. set q be quit() be my first condition, once quite the function, print the win and losses of user and computer 
+
+"""
