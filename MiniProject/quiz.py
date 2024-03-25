0ee48521bb0d6401d2b8965f2cbae5733894c7f1
Marco-yeung <shuto.yeung933@gmail.com>
2024-03-25 22:53:53 +0800

    modify quiz script

--- MiniProject/quiz.py
+++ MiniProject/quiz.py
@@ -4,14 +4,31 @@ print("Welcome to my quiz game!")
 
 answer = input("Do you want to play a game? (Y/N): ")
 
+score = 0
+
 if answer.lower() == "y":
-    answer = input("What is ILAS stand for?")
-    if answer.lower() == "Investment-Linked Assurance Scheme":
+    answer = input("What is ILAS stand for? ")
+    if answer.lower() == "investment-linked assurance scheme":
         print("Yes, You are right")
-    elif answer.lower() == "Investment Linked Assurance Scheme":
+        score += 1
+    elif answer.lower() == "investment linked assurance scheme":
         print("You are so close")
     else:
-        print("Please try again")
+        print("Here is next question")
+
+    answer = input("What is GI stand for? ")
+    if answer.lower() == "general insurance":
+        print("Yes, You are right")
+        score += 1
+    else:
+        print("Here is next question")
+
+    answer = input("What is LI stand for? ")
+    if answer.lower() == "life insurance":
+        print("Yes, You are right")
+        score += 1
+    else:
+        pass
 
 elif answer.lower() == "n":
     sys.exit("Welcome to play it next time!")
@@ -19,3 +36,6 @@ elif answer.lower() == "n":
 else:
     sys.exit("please enter valid input")
     # quit()
+
+print("ended")
+print(f"Your score is {score}")
