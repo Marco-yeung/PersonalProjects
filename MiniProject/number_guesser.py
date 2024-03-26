b5151e7489eb3b5b1d53063b34190a60af93d210
Marco-yeung <shuto.yeung933@gmail.com>
2024-03-26 08:28:58 +0800

    finish number guesser version 1 script

--- MiniProject/number_guesser.py
+++ MiniProject/number_guesser.py
@@ -5,3 +5,39 @@
 
 """
 
+import random
+
+print("Welcome to number guessing game!")
+
+answer = random.randint(0,10)
+guesses = 0
+guess_ls = []
+# isdigit() is use to test for a string is a number or not
+while True:
+    number:str = input("Enter a guess: ")  
+    while True:
+        if number.isdigit():
+            guess:int = int(number)
+            guess_ls.append(guess)
+            break
+        else:
+            print("Please enter a number")
+            number:str = input("Enter a guess: ")
+            continue
+    
+    
+    if guess == answer:
+        guesses += 1
+        print("You got it!")
+        break
+    elif guess >= answer:
+        guesses += 1
+        print("Last guess was too high")
+        print(f"here is your previous guess:", *guess_ls)
+    elif guess <= answer:
+        guesses += 1
+        print("Last guess was too low")
+        print(f"here is your previous guess: ", *guess_ls)
+print(f"The answer is {answer}, and you have guess: {guesses} times.")
+
+
