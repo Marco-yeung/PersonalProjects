ef63a6a8cb41322cf582fe28f9808ff0681eac85
Marco-yeung <shuto.yeung933@gmail.com>
2024-04-02 08:08:56 +0800

    replicate online pig, madlibs project

--- MiniProject/madlibs.py
+++ MiniProject/madlibs.py
@@ -3,3 +3,37 @@ first have a story text, with different interchangeable text surruonded with "<>
 create a function that let user to replace with there own word, make it to be a complete story
 
 """
+
+with open('story.txt', 'r') as f:
+    story = f.read()
+
+# as a list we would get duplicate elements, in order to fix this, we could use a set
+# when we are adding new elements, set would only add element only when the it is unique
+# words = []
+words = set()
+start_of_word = -1
+
+target_start = "<"
+target_end = ">"
+
+
+# enumerate() let us to have access to the position as well as the element at that position
+for i, char in enumerate(story):
+    if char == target_start: 
+        start_of_word = i
+    
+    if char == target_end and start_of_word != -1:
+         word = story[start_of_word: i+1]
+         words.add(word)
+         start_of_word = -1
+
+answers = {}
+
+for word in words:
+    answer = input(f"enter a word for {word}: ")
+    # answers["key"] = "value"
+    # for dictionary, we could access the key by using square bracket syntax like this 
+    answers[word] = answer
+
+for word in words:
+    story = story.replace(word, answers[word])
--- MiniProject/pig.py
+++ MiniProject/pig.py
@@ -3,3 +3,117 @@ create a function that let players roll a dice. make player be a variable so tha
 once there is a player reach 50, the game can ended
 
 """
+# originally thinking of function of solving the problem
+'''
+def roll() -> int: 
+    dice = random.randint(1,6)
+    return dice
+
+
+while True:
+    players = input("how many players gonna play this game (2-4)")
+    if players.isdigit():
+        players = int(players)
+        break
+    else:
+        print("Please enter valid input")
+        continue
+
+
+
+while True:
+    rolling = roll()
+    if rolling == 1:
+        break
+    else:
+        player_score += rolling
+'''
+
+# sudden think of class method
+'''
+class PIG:
+    def __init__(self, player_score):
+        self.player_score = player_score
+        player_score = 0
+
+    def roll(self) -> int: 
+        dice = random.randint(1,6)
+        return dice
+    
+    def player(self) -> int:
+        while True:
+            players = input("how many players gonna play this game (2-4)")
+            if players.isdigit():
+                players = int(players)
+                break
+            else:
+                print("Please enter valid input")
+                continue
+        return players
+    
+    def score(self) -> int:
+        while True:
+            rolling = roll()
+            if rolling == 1:
+                break
+            else:
+                player_score += rolling
+        return player_score
+    
+'''
+# then change it back to main function (oop) first, is just earier, later could try back class method
+import random
+def main():
+    player_score_ls = []
+    no_of_players = player()
+    for _ in range(no_of_players):
+        player_score_ls.append(0)
+    for i in range(len(player_score_ls)):
+        while player_score_ls[i] < 20:
+            player_score_ls[i] = score(i+1, player_score_ls[i])
+        # else:
+        #     print(f"player {i} won. his score is {player_score_ls[i]}")
+        #     break
+        
+
+
+def roll() -> int: 
+        dice = random.randint(1,6)
+        return dice
+    
+def player() -> int:
+    while True:
+        players = input("how many players gonna play this game (2-4)? ")
+        if players.isdigit():
+            players = int(players)
+            break
+        else:
+            print("Please enter valid input")
+            continue
+    return players
+
+def score(player_no, player_score) -> int:
+    while True:
+        keep_going = input(f"Player {player_no}, do you want to keep rolling another dice? (roll/hold) ").lower()
+        if keep_going == "roll":
+            rolling = roll()
+            if rolling == 1:
+                print(f"player {player_no} rolled a 1")
+                break
+            else:
+                player_score += rolling
+                print(f"Player {player_no} rolled a {rolling}. player {player_no} score: {player_score}")
+                if player_score > 20:
+                    print(f"Player {player_no} won. his score is {player_score}")
+                    break
+        elif keep_going == "hold":
+            print(f"Player {player_no}: {player_score}")
+            break
+
+        else:
+            print("Please enter valid input")
+            continue
+    return player_score
+
+if __name__ == '__main__':
+    main()
