3a7f76153f03fe224b9b29b42e19c07103d3eb95
Marco-yeung <shuto.yeung933@gmail.com>
2024-11-29 23:56:38 +0800

    update tutorial_2 and index html code

---
+++ Python Project/tech_with_tim/flask_tutorial/tutorial_2.py
@@ -0,0 +1,14 @@
+# https://www.youtube.com/watch?v=xIgPMguqyws&list=PLzMcBGfZo4-n4vJJybUVV3Un_NFS5EOgX&index=2
+
+from flask import Flask, redirect, url_for, render_template
+
+app = Flask(__name__)
+
+# function that would display the page
+# this would return to our home page, we could also say "/home"
+@app.route("/<name>")
+def home(name):
+    return render_template("index.html", content = name, r = 2)
+
+if __name__ == '__main__':
+    app.run()
